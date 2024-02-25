59e840e477e2115a79aaef47d3ab6c1c7ff2a0a6
raxNone <dryou04@kookmin.ac.kr>
2024-02-25 16:50:09 +0900

    진수 설정기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
@@ -7,29 +6,41 @@ function App() {
   const [bin,setBin] = useState(0);
   const [oct,setOct] = useState(0);
   const [hex,setHex] = useState(0);
+  const [num, setNum] = useState(10);
+  const [custom, setCustom] = useState();
+
+  let opt = [];
+  for(let i=2; i<37; i++){
+    opt.push(<option key={i}>{i}</option>)
+  }
 
   function Form1(e){
-    let t_value = e.target.value;
+    let t_value = Number(e.target.value);
     setMsg(t_value);
-    setBin(Number(t_value).toString(2));
-    setOct(Number(t_value).toString(8));
-    setHex(Number(t_value).toString(16).toUpperCase());
-    console.log(t_value);
+    setBin(t_value.toString(2));
+    setOct(t_value.toString(8));
+    setHex(t_value.toString(16).toUpperCase());
+    setCustom(t_value.toString(num).toUpperCase());
+    
   }
   
+
+
   return (
     <div>
-      <p><input type='text' id='text' onChange={Form1} /></p>
+      <p><input type='text' id='text'autoComplete='off' onChange={Form1}/></p>
       <div>
         <p>2진수 : {bin}</p>
         <p>8진수 : {oct}</p>
         <p>16진수 : {hex}</p>
+        <select style={{height:"25px", width :"50px", fontSize:"18px"}} defaultValue="10" onChange={e=>{
+          let n =Number(e.target.value);
+          setNum(n);
+          setCustom(msg.toString(n).toUpperCase())}}>{opt}</select> 진수 : {custom}
       </div>
       
     </div>
   );
 }
-
-
 
 export default App;
